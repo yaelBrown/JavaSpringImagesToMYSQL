7884376eff9cd07d6fbb91a54bcd10bd3acdbfd2
yaelBrown <yaelrbrown@gmail.com>
2019-11-07 14:13:35 -0600

    Initial setup of project.

---
+++ src/test/java/com/example/javaspringimagestomysql/JavaspringimagestomysqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaspringimagestomysql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaspringimagestomysqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
